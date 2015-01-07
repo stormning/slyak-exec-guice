46d68063d38efd0abae52d5f9cdad7189ad2f505
stormning <stormning@163.com>
2015-01-07 18:17:57 +0800

    first commit

---
+++ src/main/java/com/slyak/exec/bean/Area.java
@@ -0,0 +1,31 @@
+package com.slyak.exec.bean;
+
+import java.io.Serializable;
+
+/**
+ * .
+ * <p/>
+ *
+ * @author <a href="mailto:stormning@163.com">stormning</a>
+ * @version V1.0, 2015/1/7
+ */
+public class Area implements Serializable {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
